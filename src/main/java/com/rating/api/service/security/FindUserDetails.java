81caf5ac42c46ca651536e6db3206e6a0915753a
Kaleab-Yaye <kaleabtesfaye222@gmail.com>
2025-12-08 16:50:25 +0300

    Flyway script to add 'type' to users stored in different tables

---
+++ src/main/java/com/rating/api/service/security/FindUserDetails.java
@@ -0,0 +1,14 @@
+/*package com.rating.api.service.security;
+
+
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Service;
+
+@Service
+public class FindUserDetails implements UserDetails {
+
+
+
+}
+
+ */
---
+++ src/main/resources/db/migration/V2025120816__Added_type_to_users.sql
@@ -0,0 +1,2 @@
+ALTER TABLE pharmacists ADD COLUMN type VARCHAR(25) DEFAULT 'Pharmacist';
+ALTER TABLE inventory_managers ADD COLUMN type VARCHAR(25) DEFAULT 'InventoryManager';
