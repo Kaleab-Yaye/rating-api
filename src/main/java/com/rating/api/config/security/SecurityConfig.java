5c5bb7b4e42a73e95eb023ba0e4b59c6e0b99c78
Kaleab-Yaye <kaleabtesfaye222@gmail.com>
2025-12-09 14:14:35 +0300

    Add BCrypt encoder Bean

--- src/main/java/com/rating/api/config/security/SecurityConfig.java
+++ src/main/java/com/rating/api/config/security/SecurityConfig.java
@@ -5,6 +5,8 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration // i made sure spring boot looks at this class at start up
@@ -37,5 +39,9 @@ public class SecurityConfig {
     return httpSecurity
         .build(); // the list is build and tomcat can now iterate over eatch filters to see what to
     // do with teh request
+  }
+  @Bean
+   public PasswordEncoder passwordEncoder (){
+      return new BCryptPasswordEncoder();
   }
 }
