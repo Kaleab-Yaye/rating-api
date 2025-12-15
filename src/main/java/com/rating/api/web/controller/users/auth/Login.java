bd0ce09e184586427a3ebba81ed7a292a22d4ea1
Kaleab-Yaye <kaleabtesfaye222@gmail.com>
2025-12-15 18:33:54 +0300

    Creat Login Controller

---
+++ src/main/java/com/rating/api/web/controller/users/auth/Login.java
@@ -0,0 +1,26 @@
+package com.rating.api.web.controller.users.auth;
+
+import com.rating.api.response.AuthRequest;
+import com.rating.api.response.AuthResponse;
+import com.rating.api.service.security.AuthService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/user")
+public class Login {
+    private AuthService authService;
+
+    public void setAuthService(AuthService authService) {
+        this.authService = authService;
+    }
+    @PostMapping("/login")
+    public AuthResponse login(@RequestBody AuthRequest authRequest) {
+        return authService.login(authRequest);
+    }
+
+
+}
