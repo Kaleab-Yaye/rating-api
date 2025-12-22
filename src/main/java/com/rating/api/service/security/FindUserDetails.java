67361d0e540bf5ab39e5c56be37a0d005316cbbd
Kaleab-Yaye <kaleabtesfaye222@gmail.com>
2025-12-22 17:29:18 +0300

    made findUserDetails to handel roles in list

--- src/main/java/com/rating/api/service/security/FindUserDetails.java
+++ src/main/java/com/rating/api/service/security/FindUserDetails.java
@@ -4,7 +4,9 @@ import com.rating.api.domain.InventoryManager;
 import com.rating.api.domain.Pharmacist;
 import com.rating.api.repository.InventoryMangerRepo;
 import com.rating.api.repository.PharmacistRepo;
+import java.util.ArrayList;
 import java.util.Optional;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -25,10 +27,17 @@ public class FindUserDetails implements UserDetailsService {
     Optional<Pharmacist> optionalPharmacistpharmacist = pharmacistRepo.getPharmacistsByEmail(email);
     if (optionalPharmacistpharmacist.isPresent()) {
       Pharmacist pharmacist = optionalPharmacistpharmacist.get();
-      return org.springframework.security.core.userdetails.User.builder()
+      // to collect the roles in a do pattern way
+      ArrayList<String> role = new ArrayList<>();
+      role.add(pharmacist.getType());
+      if (pharmacist.getIsAdmin()) {
+        role.add("ADMIN");
+      }
+
+      return User.builder()
           .username(pharmacist.getId().toString()) // has to be email or ID for futre use
           .password(pharmacist.getPassword())
-          .roles(pharmacist.getType() + pharmacist.getIsAdmin().toString())
+          .roles(role.toArray(role.toArray(new String[0]))) // change the role in to an array frist
           .build();
 
     } else {
