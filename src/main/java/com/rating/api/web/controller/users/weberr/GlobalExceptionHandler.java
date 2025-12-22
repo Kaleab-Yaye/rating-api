a6c55768996ec83a01cf2e40106a680643f5f56b
Kaleab-Yaye <kaleabtesfaye222@gmail.com>
2025-12-22 15:30:10 +0300

    Creat Global ExceptionHandelr Class, Add method to hadel ArgumentNotValid Exception

---
+++ src/main/java/com/rating/api/web/controller/users/weberr/GlobalExceptionHandler.java
@@ -0,0 +1,30 @@
+package com.rating.api.web.controller.users.weberr;
+
+import java.util.List;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ProblemDetail;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+  @ExceptionHandler // could be left blank as spring will outematically much the ecption this
+  // methode is suppesed to handel
+  public ResponseEntity<ProblemDetail> handleArgumentNotValid(MethodArgumentNotValidException ex) {
+    ProblemDetail problemDetail =
+        ProblemDetail.forStatusAndDetail(
+            HttpStatus.BAD_REQUEST,
+            "Invalid details"); // an object mean tto return error in jason format
+    List<String> errors =
+        ex.getBindingResult().getFieldErrors().stream()
+            .map(err -> err.getField() + ":" + err.getDefaultMessage())
+            .toList(); // we travers along all the erros that lead to this exception and store them
+    // in list to be returend
+    problemDetail.setProperty("errors", errors); // you set the raw proproties of the error
+    return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+        .body(problemDetail); // you build the Response Enitey
+  }
+}
