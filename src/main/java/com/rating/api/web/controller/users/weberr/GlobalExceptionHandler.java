a88c83bd2040b916e26d538bb97bddb063e6ba32
Kaleab-Yaye <kaleabtesfaye222@gmail.com>
2025-12-22 15:31:41 +0300

    Add methode to handle HttpMessageNotReadableException

--- src/main/java/com/rating/api/web/controller/users/weberr/GlobalExceptionHandler.java
+++ src/main/java/com/rating/api/web/controller/users/weberr/GlobalExceptionHandler.java
@@ -4,6 +4,7 @@ import java.util.List;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ProblemDetail;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -26,5 +27,15 @@ public class GlobalExceptionHandler {
     problemDetail.setProperty("errors", errors); // you set the raw proproties of the error
     return ResponseEntity.status(HttpStatus.BAD_REQUEST)
         .body(problemDetail); // you build the Response Enitey
+  }
+
+  @ExceptionHandler
+  public ResponseEntity<ProblemDetail> handelHttpMessageNotReadableException(
+      HttpMessageNotReadableException ex) {
+    ProblemDetail problemDetail =
+        ProblemDetail.forStatusAndDetail(
+            HttpStatus.BAD_REQUEST, "Invalid JSON or Unacceptable Format");
+    problemDetail.setProperty("errors", ex.getMessage());
+    return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(problemDetail);
   }
 }
