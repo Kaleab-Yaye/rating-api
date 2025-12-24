d87a027c69d9caee431da14e318640134ff8e999
Kaleab-Yaye <kaleabtesfaye222@gmail.com>
2025-12-24 13:21:12 +0300

    Update PharacyService to Bean

--- src/main/java/com/rating/api/service/other/PharmacyService.java
+++ src/main/java/com/rating/api/service/other/PharmacyService.java
@@ -6,7 +6,7 @@ import com.rating.api.repository.PharmacyRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-
+@Service
 public class PharmacyService {
   @Autowired PharmacyRepo pharmacyRepo;
 
